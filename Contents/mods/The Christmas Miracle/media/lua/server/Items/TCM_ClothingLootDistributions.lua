6c3605be69dd4c06a0908cfd9dc0816e6f0c266a
AuthenticPeach <51739720+AuthenticPeach@users.noreply.github.com>
2022-12-20 01:59:01 -0800

    Progress 12-20-22
    Art redo
    - foods
    - xmls

---
+++ Contents/mods/The Christmas Miracle/media/lua/server/Items/TCM_ClothingLootDistributions.lua
@@ -0,0 +1,5 @@
+require 'Items/ProceduralDistributions'
+--Jacket_Warriors Distros
+table.insert(ProceduralDistributions.list["WardrobeMan"].items, "Base.Jacket_Warriors");
+table.insert(ProceduralDistributions.list["WardrobeMan"].items, 0.01);
+
